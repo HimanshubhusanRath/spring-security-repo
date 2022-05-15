73b9bd801aff50d58283371ca0984bcc5f8825ca
Himanshu Bhusan Rath <himanshubhusanrath@admins-MacBook-Pro.local>
2022-05-15 15:39:58 +0530

    comments updated

--- springboot-oauth2-impl/oauth-client/src/main/java/com/hr/oauth/client/controller/HelloController.java
+++ springboot-oauth2-impl/oauth-client/src/main/java/com/hr/oauth/client/controller/HelloController.java
@@ -2,37 +2,27 @@ package com.hr.oauth.client.controller;
 
 import java.security.Principal;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
+/*
+ * Authentication is required to access these apis. (Consent is not required here)  
+ * 
+ */
 @RestController
 @RequestMapping("/home")
 public class HelloController {
 
-	@Autowired
-	private WebClient webClient;
+	@GetMapping("/default")
+	public String defaultMessage()
+	{
+		return "defaultMessage";
+	}
 	
 	@GetMapping
 	public String hello(Principal principal) {
         return "Hello " +principal.getName()+", Welcome to Spring Boot with Oauth integration project";
     }
 	
-	@GetMapping("/users")
-	public String[] getUsers(@RegisteredOAuth2AuthorizedClient("api-client-authorization-code") OAuth2AuthorizedClient authClient)
-	{
-		System.out.println("ACCESS TOKEN >>> "+authClient.getAccessToken().getTokenValue());
-		return webClient
-					.get()
-					.uri("http://localhost:8090/secured-data/users")
-					.attributes(ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient(authClient))
-					.retrieve()
-					.bodyToMono(String[].class)
-					.block();
-	}
 }
--- springboot-oauth2-impl/oauth-client/src/main/java/com/hr/oauth/client/controller/UserController.java
+++ springboot-oauth2-impl/oauth-client/src/main/java/com/hr/oauth/client/controller/UserController.java
@@ -1,17 +1,45 @@
 package com.hr.oauth.client.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/users")
 public class UserController {
 
-	@GetMapping("/hello")
-	public String loginss()
+	@Autowired
+	private WebClient webClient;
+	
+	/*
+	 * No authentication is not required to access this api
+	 */
+	@GetMapping("/default")
+	public String defaultMessage()
 	{
 		return "Hello unauthorized user !!";
+	}
+	
+	/*
+	 * Authentication & Authorization (with consent) is required to access this api.
+	 * As the 'api-client-authorization-code' client scope is configured as 'api.read', consent will be asked during authorization
+	 */
+	@GetMapping("/user-details")
+	public String[] getUsers(@RegisteredOAuth2AuthorizedClient("api-client-authorization-code") OAuth2AuthorizedClient authClient)
+	{
+		System.out.println("ACCESS TOKEN >>> "+authClient.getAccessToken().getTokenValue());
+		return webClient
+					.get()
+					.uri("http://localhost:8090/secured-data/users")
+					.attributes(ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient(authClient))
+					.retrieve()
+					.bodyToMono(String[].class)
+					.block();
 	}
 
 }
